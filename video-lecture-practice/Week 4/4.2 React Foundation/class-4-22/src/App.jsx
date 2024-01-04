dc514c5957bf4aca9dfdb5e664038acb70916e87
shubhamvermabitscape <shubham.verma@bitscape.com>
2024-01-04 09:29:48 +0530

    React foundation done

--- video-lecture-practice/Week 4/4.2 React Foundation/class-4-22/src/App.jsx
+++ video-lecture-practice/Week 4/4.2 React Foundation/class-4-22/src/App.jsx
@@ -6,12 +6,23 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+  setInterval(async () => {
+  const res = await fetch("");
+  }, 5000);
+  // useEffect
+  // connect this to a backend
   return (
     <>
-    <div>
+    {/* <div>
       <button onClick={() => setCount((count) => count - 1)}>-</button>
       <button onClick={() => setCount((count))}>Count is {count}</button>
       <button onClick={() => setCount((count) => count + 1)}>+</button>
+    </div> */}
+
+    <div>
+      <button onClick={function() {setCount(Math.floor(Math.random()*100))}}>
+        count is {count}
+      </button>
     </div>
     </>
   )
