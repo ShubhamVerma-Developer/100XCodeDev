d3fa381acd32d1d6fab7f63e0fe586f85dfa256c
shubhamvermabitscape <shubham.verma@bitscape.com>
2024-01-08 17:38:52 +0530

    custom button component

---
+++ video-lecture-practice/Week 5/5.1 React Deep dive/counter-app/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+    <div>
+    <CustomButton count={count} setCount={setCount}></CustomButton>
+    </div>
+    </>
+  )
+
+
+  function CustomButton(props){
+    function onClickHandler(){
+      props.setCount(count + 1);
+    }
+    
+    return <button onClick={onClickHandler}>
+      Counter {props.count}
+    </button>
+  }
+}
+
+export default App
