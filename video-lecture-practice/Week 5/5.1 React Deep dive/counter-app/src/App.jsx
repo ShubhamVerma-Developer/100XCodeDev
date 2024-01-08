50ce07694d96e38e5e6044b7c572eda22c10087d
shubhamvermabitscape <shubham.verma@bitscape.com>
2024-01-08 18:55:22 +0530

    sending props

--- video-lecture-practice/Week 5/5.1 React Deep dive/counter-app/src/App.jsx
+++ video-lecture-practice/Week 5/5.1 React Deep dive/counter-app/src/App.jsx
@@ -2,28 +2,45 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([
+    {
+    title: "Go to gym",
+    description: "Go to gym from 7-9",
+    completed: false
+  },
+    {
+    title: "Study DSA",
+    description: "Go to gym from 9-100",
+    completed: true
+  },
+
+]);
+
   return (
     <>
     <div>
-    <CustomButton count={count} setCount={setCount}></CustomButton>
-    <CustomButton count={count +1} setCount={setCount}></CustomButton>
-    <CustomButton count={count -1} setCount={setCount}></CustomButton>
-    <CustomButton count={count + 100} setCount={setCount}></CustomButton>
+    <Todo title="shubham" description="software developer" completed="false"></Todo>
+    <CustomBotton count="41"></CustomBotton>
     </div>
     </>
   )
 
 
-  function CustomButton(props){
-    function onClickHandler(){
-      props.setCount(count + 1);
-    }
-    
-    return <button onClick={onClickHandler}>
-      Counter {props.count}
-    </button>
+  function Todo(props){
+    return <div>
+      <h1>{props.title}</h1>
+      <h1>{props.description}</h1>
+      <h1>{props.completed}</h1>
+    </div>
   }
+
+  function CustomBotton(props){
+    return <div>
+      <h1>{props.count}</h1>
+    </div>
+
+  }
+  
 }
 
 export default App
