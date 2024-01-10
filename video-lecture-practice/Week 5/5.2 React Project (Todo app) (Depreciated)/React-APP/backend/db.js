4e91204184244225913be456b5b4a7bcd5e718ee
shubhamvermabitscape <shubham.verma@bitscape.com>
2024-01-10 11:11:34 +0530

    mongo DB backend connect postman .env file connection string

---
+++ video-lecture-practice/Week 5/5.2 React Project (Todo app) (Depreciated)/React-APP/backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+// mongodb+srv://kirags123:8qPEa8KTKBEh2bss@cluster0.f3qlbuo.mongodb.net/todos
+// .env
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+mongoose.connect(connectionString);
+console.log(connectionString);
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+const todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo,
+};
