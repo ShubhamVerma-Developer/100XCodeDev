8336edb3fe1b93d30ae033c38bc40bec1f854990
Shubham25122001 <shubham25122001@gmail.com>
2024-01-17 12:24:52 +0530

    setInterval to render the todos

---
+++ video-lecture-practice/Week 6/6.1 React hooks/Hooks/src/App.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+
+function App() {
+  let [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+      setInterval(() => {
+        fetch("https://sum-server.100xdevs.com/todos")
+        .then(async function(res){
+          const json = await res.json();
+          setTodos(json.todos);
+        })
+      }, 10000)
+    }, []); 
+
+    return <div>
+    {todos.length}
+  {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description} />)}
+  </div>
+}
+
+function Todo({title, description}){
+  return <div>
+    <h1>
+      {title}
+    </h1>
+    <h4>
+      {description}
+    </h4>
+  </div>
+}
+
+export default App
