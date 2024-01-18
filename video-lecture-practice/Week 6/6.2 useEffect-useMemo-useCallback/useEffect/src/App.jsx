7245cd8b0b8ffac468935540fd040b4cfd6820f1
Shubham25122001 <shubham25122001@gmail.com>
2024-01-18 13:10:51 +0530

    button click todo render assignment done from my side

--- video-lecture-practice/Week 6/6.2 useEffect-useMemo-useCallback/useEffect/src/App.jsx
+++ video-lecture-practice/Week 6/6.2 useEffect-useMemo-useCallback/useEffect/src/App.jsx
@@ -3,30 +3,54 @@ import axios from "axios";
 import './App.css'
 
 function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("https://sum-server.100xdevs.com/todos")
-      .then(function (response) {
-        setTodos(response.data.todos);
-      });
-  }, []);
-
-  console.log(todos);
+  const [todoId, setTodoId] = useState(1);
   return (
-    <>
-    {todos.map((todo)=> <Todo key={todo.id} id = {todo.id} title={todo.title} description={todo.description} />)}
-    </>
+    <div>
+      <button onClick={() => { setTodoId(1) }}>1</button>
+      <button onClick={() => { setTodoId(2) }}>2</button>
+      <button onClick={() => { setTodoId(3) }}>3</button>
+      <button onClick={() => { setTodoId(4) }}>4</button>
+      <button onClick={() => { setTodoId(5) }}>5</button>
+      <Todo id={todoId} />
+    </div>
   );
 }
 
-function Todo({id, title, description}){
+function FetchAllTodo(){
+  const [todos, setTodos] =  useState();
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todos")
+    .then(response => {
+      setTodos(response.data.todos);
+      console.log(todos);
+    })
+  }, [])
+  return todos.length;
+}
+
+function Todo({id}) {
+  const [todo, setTodo] = useState({});
+
+  useEffect(() => {
+    axios.get("https://sum-server.100xdevs.com/todo?id="+id)
+   .then(response => {
+    setTodo(response.data.todo)
+   })
+  }, [id])
+
+
   return <div>
-    {id} :  
-     {title} : 
-     {description}
+    <h1>{todo.id}</h1>
+    <h1>
+    {todo.title}
+    </h1>
+    <h4>
+      {todo.description}
+    </h4>
   </div>
 }
+
+
+
 
 export default App;
